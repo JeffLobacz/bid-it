e56db85450037193f9f0f5f05d8cf3babcee11e9
Jeff <jeff.lobacz@gmail.com>
2016-09-24 20:13:52 -0700

    Completed add employee view

--- app/models/seniority.rb
+++ app/models/seniority.rb
@@ -2,4 +2,8 @@ class Seniority < ActiveRecord::Base
 
   scope :sort_by_bid_number, -> { order(bid_number: :asc) }
 
+  # def self.qualification
+  #   ["Expeditor", "a", "b", "c", "d", "e"]
+  # end
+
 end
