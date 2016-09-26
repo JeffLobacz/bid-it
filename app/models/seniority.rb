58192deeb7e22aa8e5c996734e48966b46a6bc1b
Jeff <jeff.lobacz@gmail.com>
2016-09-26 11:03:00 -0700

    Fixed formatting

--- app/models/seniority.rb
+++ app/models/seniority.rb
@@ -2,8 +2,4 @@ class Seniority < ActiveRecord::Base
 
   scope :sort_by_bid_number, -> { order(bid_number: :asc) }
 
-  # def self.qualification
-  #   ["Expeditor", "a", "b", "c", "d", "e"]
-  # end
-
 end
