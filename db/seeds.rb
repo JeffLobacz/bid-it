22d282a497084157cfabb18383b4d73baf1ae14c
Jeff <jeff.lobacz@gmail.com>
2016-10-08 01:21:53 -0700

    Seeds working for all days

--- db/seeds.rb
+++ db/seeds.rb
@@ -92,29 +92,77 @@ seniorities = Seniority.all
 
 # Create CC Shifts
 shift_number = 1
-
+  # watched
+watched_array = [true, false]
+  # Days OFF
+days_off_array = %w[SM MT TW WQ QF FJ JS]
+  # Start Times
 start_times_array = []
 start = Time.new(2016, 10, 1, 21, 0, 0)
-20.times do
+(8*4).times do
   start_times_array << start
   start += 15.minutes
 end
-# quit = start + 8.hours + 30.minutes
 locations = %w[40 42A 42B 46A 46B 46C 48A 48B 49B 49A 47B 47A 45 43 41]
 5.times do
-  start_time = start_times_array[rand(start_times_array.length)]
-  quit_time = start_time + (8.5*60*60)
+  weekend = days_off_array[rand(days_off_array.length)]
+  unless weekend == "JS" || weekend == "SM"
+    sun_start = start_times_array[rand(start_times_array.length)]
+    sun_quit = sun_start + (8.5*60*60)
+  end
+  unless weekend == "SM" || weekend == "MT"
+    mon_start = start_times_array[rand(start_times_array.length)]
+    mon_quit = mon_start + (8.5*60*60)
+  end
+  unless weekend == "MT" || weekend == "TW"
+    tue_start = start_times_array[rand(start_times_array.length)]
+    tue_quit = tue_start + (8.5*60*60)
+  end
+  unless weekend == "TW" || weekend == "WQ"
+    wed_start = start_times_array[rand(start_times_array.length)]
+    wed_quit = wed_start + (8.5*60*60)
+  end
+  unless weekend == "WQ" || weekend == "QF"
+    thu_start = start_times_array[rand(start_times_array.length)]
+    thu_quit = thu_start + (8.5*60*60)
+  end
+  unless weekend == "QF" || weekend == "FJ"
+    fri_start = start_times_array[rand(start_times_array.length)]
+    fri_quit = fri_start + (8.5*60*60)
+  end
+  unless weekend == "FJ" || weekend == "JS"
+    sat_start = start_times_array[rand(start_times_array.length)]
+    sat_quit = sat_start + (8.5*60*60)
+  end
   Shift.create!(
+    watched:                watched_array[rand(watched_array.length)],
     main_location:          "Ramp",
     detail_location:        "Gates",
     shift_number:           shift_number,
     employment_status:      "CC",
-    sun_start_time:         start_time,
-    sun_quit_time:          quit_time,
+    days_off:               weekend,
+    sun_start_time:         sun_start,
+    sun_quit_time:          sun_quit,
     sun_location:           locations[rand(locations.length)],
+    mon_start_time:         mon_start,
+    mon_quit_time:          mon_quit,
+    mon_location:           locations[rand(locations.length)],
+    tue_start_time:         tue_start,
+    tue_quit_time:          tue_quit,
+    tue_location:           locations[rand(locations.length)],
+    wed_start_time:         wed_start,
+    wed_quit_time:          wed_quit,
+    wed_location:           locations[rand(locations.length)],
+    thu_start_time:         thu_start,
+    thu_quit_time:          thu_quit,
+    thu_location:           locations[rand(locations.length)],
+    fri_start_time:         fri_start,
+    fri_quit_time:          fri_quit,
+    fri_location:           locations[rand(locations.length)],
+    sat_start_time:         sat_start,
+    sat_quit_time:          sat_quit,
+    sat_location:           locations[rand(locations.length)],
   )
-  # start += 30.minutes
-  # quit = start + 8.hours + 30.minutes
   shift_number += 1
 end
 
