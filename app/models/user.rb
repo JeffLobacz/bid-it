0d4191f545ed372306c3f04820bed8301c0cd946
Jeff <jeff.lobacz@gmail.com>
2017-04-08 02:28:37 -0700

    minor adjustments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,31 +3,32 @@ class User < ActiveRecord::Base
   # scope :sort_by_bid_number, -> { order(bid_number: :asc) }
   scope :sort_by_seniority, -> { order(seniority: :asc) }
 
-  after_update :mark_shift_taken
 
-  validates_presence_of :shift, numericality: true, on: :update
+  validates_presence_of :shift, numericality: true, on: :update, :if => :shift_ok?
   #
-  # def shift_ok
-  #   if shift <= 0 || shift > Shift.count || Shift.find_by(shift_number: shift).employee_number != nil
-  #     # User.errors.messages(:shift, "Sorry, shift not within range or already taken. Try again with different selection.")
-  #   end
-  # end
+  def shift_ok?
+    if shift <= 0 || shift > Shift.count || Shift.find_by(shift_number: shift).employee_number != nil
+      errors.add(:shift, "Sorry, shift not within range or already taken. Try again with different selection.")
+    end
+  end
 
+  # after_update :mark_shift_taken
 
   private
 
-  def mark_shift_taken
-    @schedule = Shift.find_by(shift_number: @employee.shift)
-    @schedule.assign_attributes(employee_number: @employee.employee_number)
-    @schedule.assign_attributes(last_name: @employee.last_name)
-    @schedule.assign_attributes(first_name: @employee.first_name)
-    if @schedule.save
-      flash[:notice] = "Shift table was updated."
-      redirect_to user_path
-    else
-      flash[:error] = "There was an error updating the Shift table. Please try again."
-      render :edit
-    end
-  end
+  # def mark_shift_taken
+  #   user = User.find_by(shift: :shift)
+  #   @schedule = Shift.find_by(shift_number: user.bid_number)
+  #   @schedule.assign_attributes(employee_number: user.employee_number)
+  #   @schedule.assign_attributes(last_name: user.last_name)
+  #   @schedule.assign_attributes(first_name: user.first_name)
+  #   if @schedule.save
+  #     flash[:notice] = "Shift table was updated."
+  #     redirect_to user_path
+  #   else
+  #     flash[:error] = "There was an error updating the Shift table. Please try again."
+  #     render :edit
+  #   end
+  # end
 
 end
